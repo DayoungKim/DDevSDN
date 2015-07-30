8269dcad96bbe42f3c3a8c237bf4d332fc627a10
Kim Dayoung <dayoung.kim@kulcloud.net>
2015-07-30 10:45:52 +0900

    openmul

---
+++ ddevsdn.sh
@@ -0,0 +1,153 @@
+#!/usr/bin/env bash
+
+# DDevSDN for Ubuntu 
+# Dayoung Kim (kdy906@naver.com)
+
+# Fail on error
+set -e
+
+# Fail on unset var usage
+set -o nounset
+
+# Get directory of DDevSDN folder
+ROOT_DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P )"
+
+# Set up build directory, which by default is the working directory
+#  unless the working directory is a subdirectory of mininet, 
+#  in which case we use the directory containing mininet
+BUILD_DIR="$(pwd -P)"
+case $BUILD_DIR in
+  $ROOT_DIR/*) BUILD_DIR=$ROOT_DIR;; # currect directory is a subdirectory
+  *) BUILD_DIR=$BUILD_DIR;;
+esac
+
+# Attempt to identify Linux release
+
+DIST=Unknown
+RELEASE=Unknown
+CODENAME=Unknown
+ARCH=`uname -m`
+if [ "$ARCH" = "x86_64" ]; then ARCH="amd64"; fi
+if [ "$ARCH" = "i686" ]; then ARCH="i386"; fi
+
+test -e /etc/debian_version && DIST="Debian"
+grep Ubuntu /etc/lsb-release &> /dev/null && DIST="Ubuntu"
+if [ "$DIST" = "Ubuntu" ] || [ "$DIST" = "Debian" ]; then
+    install='sudo apt-get -y install'
+    remove='sudo apt-get -y remove'
+    pkginst='sudo dpkg -i'
+    # Prereqs for this script
+    if ! which lsb_release &> /dev/null; then
+        $install lsb-release
+    fi
+fi
+test -e /etc/fedora-release && DIST="Fedora"
+if [ "$DIST" = "Fedora" ]; then
+    install='sudo yum -y install'
+    remove='sudo yum -y erase'
+    pkginst='sudo rpm -ivh'
+    # Prereqs for this script
+    if ! which lsb_release &> /dev/null; then
+        $install redhat-lsb-core
+    fi
+fi
+if which lsb_release &> /dev/null; then
+    DIST=`lsb_release -is`
+    RELEASE=`lsb_release -rs`
+    CODENAME=`lsb_release -cs`
+fi
+echo "Detected Linux distribution: $DIST $RELEASE $CODENAME $ARCH"
+
+# Kernel params
+
+KERNEL_NAME=`uname -r`
+KERNEL_HEADERS=kernel-headers-${KERNEL_NAME}
+
+if ! echo $DIST | egrep 'Ubuntu|Debian|Fedora'; then
+    echo "ddevsdn.sh currently only supports Ubuntu, Debian and Fedora."
+    exit 1
+fi
+
+# More distribution info
+DIST_LC=`echo $DIST | tr [A-Z] [a-z]` # as lower case
+
+
+# Determine whether version $1 >= version $2
+# usage: if version_ge 1.20 1.2.3; then echo "true!"; fi
+function version_ge {
+    # sort -V sorts by *version number*
+    latest=`printf "$1\n$2" | sort -V | tail -1`
+    # If $1 is latest version, then $1 >= $2
+    [ "$1" == "$latest" ]
+}
+
+# Install OpenMUL
+function openmul {
+    echo "Installing MUL Controller..."
+
+    cd $BUILD_DIR/
+    git clone https://github.com/openmul/openmul.git
+
+    cd openmul
+    ./build.sh
+
+    cp application/nbapi/c-swig/.lib/_mul_nbapi.so application/nbapi/py-tornado/app/lib/
+    cp application/nbapi/c-swig/.lib/_mul_nbapi.so.0 application/nbapi/py-tornado/app/lib/
+    cp application/nbapi/c-swig/.lib/_mul_nbapi.so.0.0.0 application/nbapi/py-tornado/app/lib/
+    cp application/nbapi/c-swig/.lib/mul_nbapi.py application/nbapi/py-tornado/app/lib/
+
+    cp application/nbapi/c-swig/.lib/_mul_nbapi.so TEST_SCRIPTS/python/
+    cp application/nbapi/c-swig/.lib/_mul_nbapi.so.0 TEST_SCRIPTS/python/
+    cp application/nbapi/c-swig/.lib/_mul_nbapi.so.0.0.0 TEST_SCRIPTS/python/
+    cp application/nbapi/c-swig/.lib/mul_nbapi.py TEST_SCRIPTS/python/
+
+    cd $BUILD_DIR
+}
+
+function usage {
+    echo $BUILD_DIR
+    printf '\nUsage: %s [-acdfhimnorv]\n\n' $(basename $0) >&2
+
+    printf 'This DDevSDN script attempts to install, run and testing useful software\n' >&2
+    printf 'for OpenMUL.It should (hopefully) work on Ubuntu 11.10+\n' >&2
+    printf 'If you run into trouble, try\n' >&2
+    printf 'installing one thing at a time, and looking at the \n' >&2
+    printf 'specific installation function in this script.\n\n' >&2
+
+    printf 'options:\n' >&2
+    printf -- ' -a: -install (A)vior, sdn gui\n' >&2
+    printf -- ' -b: -install controller (B)enchmark (oflops)\n' >&2
+    printf -- ' -d: -install Open(D)ayLight controller\n' >&2
+    printf -- ' -f: -install (F)loodlight controller\n' >&2
+    printf -- ' -i: -install M(I)ninet \n' >&2
+    printf -- ' -m: -install open(M)ul controller\n' >&2
+    printf -- ' -n: -install O(N)os controller\n' >&2
+    printf -- ' -o: -install (O)penflow \n' >&2
+    printf -- ' -r: -install (R)yu controller\n' >&2
+    printf -- ' -v: -install open(V)switch\n' >&2
+
+    exit 2
+}
+
+if [ $# -eq 0 ]
+then
+    usage
+else
+    while getopts 'abdfimnorv' OPTION
+    do
+      case $OPTION in
+      #a)    avior;;
+      #b)    cbench;;
+      #d)    opendaylight;;
+      #f)    floodlight;;
+      #i)    mininet;;
+      m)    openmul;;
+      #n)    onos;;
+      #o)    openflow;;
+      #r)    ryu;;
+      #v)    openvswitch;;
+      ?)    usage;;
+      esac
+    done
+    shift $(($OPTIND - 1))
+fi
