3379a12534ead8f4f3b0786de6e6f0d0134a5e17
Kim Dayoung <dayoung.kim@kulcloud.net>
2015-09-16 17:32:03 +0900

    installing npm for avior added

--- ddevsdn.sh
+++ ddevsdn.sh
@@ -90,6 +90,7 @@ function avior {
     sudo apt-get install python-software-properties python g++ make
     sudo add-apt-repository ppa:chris-lea/node.js
     sudo apt-get install nodejs
+    sudo npm -g install sails
 }
 # Install Cbench
 function cbench {
